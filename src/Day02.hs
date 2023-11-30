66922aa887dcb482d1be8f316b9a4fed01e23d1b
Pierre Thierry <pierre@nothos.net>
2023-11-30 16:38:54 +0100

    Add the ability to run part of a day

--- src/Day02.hs
+++ src/Day02.hs
@@ -4,7 +4,19 @@ import Paths_aoc2023 (getDataFileName)
 
 day02 :: IO ()
 day02 = do
-  inputLines <- lines <$> (getDataFileName "day02-input.txt" >>= readFile)
+  day02_1
+  day02_2
+
+day02_1 :: IO ()
+day02_1 = do
+  inputLines <- lines <$> (getDataFileName "day02-1-input.txt" >>= readFile)
   putStrLn "This is what I read from input:"
   putStrLn $ unlines inputLines
-  putStrLn "TODO: implement Day 02"
+  putStrLn "TODO: implement Day 02 part 1"
+
+day02_2 :: IO ()
+day02_2 = do
+  inputLines <- lines <$> (getDataFileName "day02-2-input.txt" >>= readFile)
+  putStrLn "This is what I read from input:"
+  putStrLn $ unlines inputLines
+  putStrLn "TODO: implement Day 02 part 2"
